8047e8e2dd0a6ac8f5bc52c608052baeb736711c
Marina Atanasova <marinakzaharieva@gmail.com>
2026-03-13 20:38:47 +0200

    Reset all migration files and updated settings.py to connect to pssql without .env folder. Placeholder db values are in settings.py file now

--- DevHomesDjango/settings.py
+++ DevHomesDjango/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'DevHomesDjango.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": 'latestdevtest',
+        "USER": 'postgres',
+        "PASSWORD": '123456',
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
